5833f2b48f05dd00284964a12985e8d029293e6e
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-27 15:44:58 +0100

    add dot select media

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const app = new Vue({
     el: '#root',
     data: {
         currentMedia: 0,
+        isActive: false,
         medias: [
             'images/image1.jpg',
             'images/image2.jpg',
@@ -16,6 +17,9 @@ const app = new Vue({
     methods: {
         changeMedia(index) {
             return this.currentMedia === index;
+        },
+        setMedia(index) {
+            this.currentMedia = index
         }
     }
 })
