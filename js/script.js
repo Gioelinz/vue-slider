fcbfc0c8d378610c4bd687b12ede7e3f792646d6
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-27 15:14:55 +0100

    add condition for images

--- js/script.js
+++ js/script.js
@@ -5,8 +5,17 @@ Vue.config.devtools = true;
 const app = new Vue({
     el: '#root',
     data: {
+        currentMedia: 0,
         medias: [
             'images/image1.jpg',
-        ]
+            'images/image2.jpg',
+            'images/image3.jpg',
+            'images/image4.jpg',
+        ],
+    },
+    methods: {
+        changeMedia(index) {
+            return this.currentMedia === index;
+        }
     }
 })
